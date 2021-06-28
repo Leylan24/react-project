4f65c1c2304d54e2f576dbcc8a931c538842b839
Leylan Aurakeasamy <leylan.aurakeasamy1@gmail.com>
2021-06-28 23:08:57 +0200

    Added increase and decrease buttons

--- src/Counter.js
+++ src/Counter.js
@@ -1,10 +1,31 @@
 import React,{Component} from "react";
 
 class Counter extends Component{
+    constructor(props){ // runs when component is mounted
+        super(props);
+        this.state={
+            count: 0
+        };
+    }
+
+    increase = () =>{
+        this.setState({
+            count:this.state.count + 1
+        })
+    };
+
+    decrease = () =>{
+        this.setState({
+            count:this.state.count - 1
+        })
+    };
+
     render(){
         return(
             <div>
-                <p>I dont want to be no hero! </p>
+                <p>Current Count:{this.state.count} </p>
+                <button onClick={this.increase}>+</button>
+                <button onClick={this.decrease}>-</button>
             </div>
         )
     }
