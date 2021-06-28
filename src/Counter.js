dd77760b7fbecef6c51f201521e5e07549a07386
Leylan Aurakeasamy <leylan.aurakeasamy1@gmail.com>
2021-06-28 22:53:33 +0200

    Added counter js file

---
+++ src/Counter.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+
+class Counter extends Component{
+    render(){
+        return(
+            <div>
+                <p>I dont want to be no hero! </p>
+            </div>
+        )
+    }
+}
+
+export default Counter; // Allows the class to be exported and used by other classes
